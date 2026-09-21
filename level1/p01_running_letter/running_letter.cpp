5a6819912bdb51d667156240b98c2cb1aef3d3b5
夨宆圯厎 <923266558@qq.com>
2026-09-21 18:42:39 +0800

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch main # Your branch is ahead of 'origin/main' by 1 commit. #   (use "git push" to publish your local commits) # # Changes to be committed: #	modified:   level1/p01_running_letter/running_letter.cpp #

--- level1/p01_running_letter/running_letter.cpp
+++ level1/p01_running_letter/running_letter.cpp
@@ -5,17 +5,30 @@
 #include<conio.h>
 using namespace std;
 #define waittime 7000
-#define sleeptime 200
-int getwidth()
+#define sleeptime 50
+int n,W,H;char c;
+struct letter
+{
+    int x,y,o;//o为状态
+}Head,End;
+HANDLE handle_output=GetStdHandle(STD_OUTPUT_HANDLE);
+void hide_cursor()//隐藏光标
+{
+    CONSOLE_CURSOR_INFO a;
+    a.bVisible=0;
+    a.dwSize=1;
+    SetConsoleCursorInfo(handle_output,&a);
+}
+int get_width()
 {
     CONSOLE_SCREEN_BUFFER_INFO a;
-    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE),&a);
+    GetConsoleScreenBufferInfo(handle_output,&a);
     return a.srWindow.Right-a.srWindow.Left+1;
 }
-int getheight()
+int get_height()
 {
     CONSOLE_SCREEN_BUFFER_INFO a;
-    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE),&a);
+    GetConsoleScreenBufferInfo(handle_output,&a);
     return a.srWindow.Bottom-a.srWindow.Top+1;
 }
 char input()//限时输入
@@ -29,52 +42,74 @@ char input()//限时输入
     }
     return 0;
 }
-int n,c;
-struct letter
+/*void gotoxy(int x,int y)//移动光标
 {
-    int x,y,o;//o为状态
-};
-void letterwent_output(int n,char c)
+    COORD a;
+    a.X=x;a.Y=y;
+    SetConsoleCursorPosition(handle_output,a);
+}*/
+void erase_output(int x_1,int y_1,int x_2,int y_2)//擦除输出
 {
-    int w=getwidth()-1,h=getheight()-1;//获取控制台窗口宽高
-    letter *a=(letter*)malloc(sizeof(letter)*n);
-    for(int i=0;i<n;i++)
-        a[i].x=i,a[i].y=0,a[i].o=1;
+    COORD a;DWORD b;
+    a.X=x_1;a.Y=y_1;
+    FillConsoleOutputCharacter(handle_output,' ',1,a,&b);
+    a.X=x_2;a.Y=y_2;
+    FillConsoleOutputCharacter(handle_output,c,1,a,&b);
+}
+void letter_went()
+{
     while(1)
     {
-        system("cls");
-        for(int i=0;i<n;i++)
-        {
-            if(a[i].o==1) a[i].x++;
-            else if(a[i].o==2) a[i].y++;
-            else if(a[i].o==3) a[i].x--;
-            else if(a[i].o==4) a[i].y--;
-            if(a[i].x==w) a[i].o=2;
-            else if(a[i].y==h) a[i].o=3;
-            else if(a[i].x==0) a[i].o=4;
-            else if(a[i].y==0) a[i].o=1;
-        }
-        sort(a,a+n,[](letter a,letter b){return a.x<b.x?true:a.y<b.y?true:false;});
-        for(int i=0;i<n;)
-        {
-            
-        }
+        if(Head.o==1) Head.x++;
+        else if(Head.o==2) Head.y++;
+        else if(Head.o==3) Head.x--;
+        else if(Head.o==4) Head.y--;
+        if(Head.x==W&&Head.o==1) Head.o=2;
+        else if(Head.y==H&&Head.o==2) Head.o=3;
+        else if(Head.x==0&&Head.o==3) Head.o=4;
+        else if(Head.y==0&&Head.o==4) Head.o=1;
+        erase_output(End.x,End.y,Head.x,Head.y);
+        if(End.o==1) End.x++;
+        else if(End.o==2) End.y++;
+        else if(End.o==3) End.x--;
+        else if(End.o==4) End.y--;
+        if(End.x==W&&End.o==1) End.o=2;
+        else if(End.y==H&&End.o==2) End.o=3;
+        else if(End.x==0&&End.o==3) End.o=4;
+        else if(End.y==0&&End.o==4) End.o=1;
         Sleep(sleeptime);
-    }
-}
-void GO()
-{
-    printf("Please input the number(1~9) of letters within %d seconds.If you do not input,it will be 7\n",waittime/1000);
-    n=input();
-    if(n==0) n=7;
-    else n=n-'0';
-    printf("Please input the letter you want to run within %d seconds.If you do not input,it will be '7'\n",waittime/1000);
-    c=input();
-    if(c==0) c='7';
-    letterwent_output(n,c);
+        if(_kbhit()) if(_getch()==27) break;
+    };
 }
 int main()
 {
-    GO();
+    system("cls");
+    hide_cursor();
+    W=get_width()-1;H=get_height()-1;//获取控制台窗口宽高
+    printf("Please input the number(1~9) of letters within %d seconds.If you do not input,it will be 7.\n",waittime/1000);
+    n=input();
+    if(n==0) n=7;
+    else n=n-'0';
+    printf("Please input the letter you want to run within %d seconds.If you do not input,it will be '7'.\n",waittime/1000);
+    c=input();
+    if(c==0) c='7';
+    printf("Don't change anything about the console window.\nWhen it is running, you can press the 'ESC' key to exit the program.");
+    Sleep(waittime);
+    system("cls");
+    End.x=End.y=Head.x=Head.y=0;End.o=Head.o=1;
+    for(int i=1;i<n;i++)
+    {
+        if(Head.o==1) Head.x++;
+        else if(Head.o==2) Head.y++;
+        else if(Head.o==3) Head.x--;
+        else if(Head.o==4) Head.y--;
+        if(Head.x==W) Head.o=2;
+        else if(Head.y==H) Head.o=3;
+        else if(Head.x==0) Head.o=4;
+        else if(Head.y==0) Head.o=1;
+        erase_output(0,0,Head.x,Head.y);
+    }
+    erase_output(0,0,0,0);
+    letter_went();//开跑
     return 0;
-}
+}//其实还可以写定义颜色，但是我懒
